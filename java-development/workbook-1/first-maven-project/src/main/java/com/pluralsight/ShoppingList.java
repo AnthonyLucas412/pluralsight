d53c2804a0eb7078e267f6ed80f01f408a16987d
AnthonyLucas412 <anthonylucas4123@gmail.com>
2024-04-11 13:54:36 -0400

    initial commit

---
+++ java-development/workbook-1/first-maven-project/src/main/java/com/pluralsight/ShoppingList.java
@@ -0,0 +1,16 @@
+package com.pluralsight;
+
+public class ShoppingList {
+    public static void main(String[] args) {
+        System.out.println("My Shopping List");
+        System.out.println("milk");
+        System.out.println("cookies");
+        System.out.println("eggs");
+        System.out.println("biscuits");
+        System.out.println("bread");
+        System.out.println("butter");
+        System.out.println("cereal");
+        System.out.println("ground meat");
+        System.out.println("turkey");
+    }
+}
